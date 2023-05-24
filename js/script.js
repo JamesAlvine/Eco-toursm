0533b1a65b07143e066db234b6261d47147d873c
James Alvine <james.alvine@xc360.co.ke>
2023-05-24 08:26:45 +0300

    pic update

--- js/script.js
+++ js/script.js
@@ -28,17 +28,17 @@ document.querySelector('#close-search').onclick = ()=>{
 window.onscroll=()=>{
     navbar.classList.remove('active');
 
-    if (window.scrollY>0) {
-        document.querySelector('header').classList.add('active');
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
         
     } else {
-        document.querySelector('header').classList.remove('active');
+        document.querySelector('.header').classList.remove('active');
         
     }
 };
 
 window.onload=()=>{
-    if (window.scrollY>0){
+    if (window.scrollY > 0){
         document.querySelector('.header').classList.add('active');
         
     }else{
