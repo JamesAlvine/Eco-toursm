dedc12dff93d674e910de874a23faf9afb55d089
James Alvine <james.alvine@xc360.co.ke>
2023-06-02 19:37:13 +0300

    shop

--- js/script.js
+++ js/script.js
@@ -66,11 +66,26 @@ var swiper = new Swiper(".home-slider", {
   var swiper = new Swiper(".home-slider", {
     loop: true,
     grabCursor: true,
-    cssMode: true,
+    cssMode: true,    
+    spaceBetween: 20,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+          },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
     pagination: {
       el: ".swiper-pagination",
     },
