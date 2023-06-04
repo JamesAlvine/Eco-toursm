65c02622fd84b5339599cb2d546c4dacd8aadd70
jamesalvine <jamesalvine340@gmail.com>
2023-06-04 14:57:20 +0300

    final updates01

--- js/script.js
+++ js/script.js
@@ -88,4 +88,67 @@ var swiper = new Swiper(".home-slider", {
     },
   });
 
+  var swiper = new Swiper(".review-slide", {
+    loop: true,
+    grabCursor: true,
+    cssMode: true,    
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+    },
+  }); 
 
+  var swiper = new Swiper(".blogs-slide", {
+    loop: true,
+    grabCursor: true,
+    cssMode: true,    
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 4,
+      },
+    },
+  }); 
+
+  var swiper = new Swiper(".partners-slider", {
+    loop: true,
+    grabCursor: true,
+    cssMode: true,    
+    spaceBetween: 20,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
